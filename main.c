cb3b80861a583ec6c86edf0bdbdf1223dc1fddc0
coco230415 <es020608@sookmyung.ac.kr>
2024-12-20 01:41:52 +0900

    코드 작성 완료

--- main.c
+++ main.c
@@ -24,14 +24,19 @@ int main() {
     HealthData health_data = {0};
     
     // Tocode: to read the list of the exercises and diets
-    
+    loadExercises(EXERCISEFILEPATH);
+    loadDiets(DIETFILEPATH);
 
+    int remaining_calories;
+    remaining_calories = (health_data.total_calories_intake) - BASAL_METABOLIC_RATE - (health_data.total_calories_burned);
+    
     // ToCode: to run the "Healthcare Management Systems" until all calories are used up or the user wants to exit the system
+    //모든 칼로리 소비되거나 사용자가 시스템 끝내길 바랄때까지 동작함  
     do {
-    	if ( ){
+    	if (remaining_calories == 0){                                     //남아있는 칼로리가 0이 되면 모든 칼로리 소비했다고 출력됨  
             printf("You have consumed all your calories for today! \n");
 		} 
-		else{
+		else{                                                             //0이 아니면 옵션 뜸  
 			printf("\n=======================================================================\n");
         	printf("[Healthcare Management Systems] \n");
         	printf("1. Exercise \n");
@@ -44,21 +49,21 @@ int main() {
         }
         
 		// ToCode: to run the sysmtem based on the user's choice
+		//사용자의 선택에 따른 시스템 동작 
         switch (choice) {
             case 1:
-            	
+            	inputExercise(&health_data); 
                 break;
                 
             case 2:
-            	
+            	inputDiet(&health_data); 
                 break;
                 
             case 3:
-            	
-                break;
+            	printHealthData(&health_data);
+		        break;
                 
             case 4:
-            	
     			printf("Exit the system.\n");
     			printf("=======================================================================\n");
                 break;
@@ -67,7 +72,7 @@ int main() {
                 printf("[Error] Invalid option. \n");
                 printf("Please try again! \n");
         }
-    } while ( );
+    } while (choice != 4);
 
     return 0;
 }
